0462c0983f7f16a43653f16c6c2570cdf37ffd58
Harm Aarts <harm@aarts.email>
2020-01-06 09:40:53 +0100

    fmt

--- example/blockchair_example.dart
+++ example/blockchair_example.dart
@@ -5,7 +5,7 @@ main() async {
   var client = Blockchair('https://api.blockchair.com/bitcoin/');
   print(await client.stats());
 
-  print(await client.blocks([1,2]));
+  print(await client.blocks([1, 2]));
 
   client.close();
 }
